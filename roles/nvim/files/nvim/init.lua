1c60bc92eff170758edc410254bea3f39357517a
MrChristianL <mrchristianleonard@gmail.com>
2025-03-24 18:35:51 -0500

    update init.lua

--- roles/nvim/files/nvim/init.lua
+++ roles/nvim/files/nvim/init.lua
@@ -1,2 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+vim.opt.relativenumber = false -- Turn off relative line numbers
+vim.opt.number = true -- Keep absolute line numbers
+vim.opt.list = false -- Disables `listchars` completely
